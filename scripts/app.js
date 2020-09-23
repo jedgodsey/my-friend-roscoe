32c1734c8a697674ada8c87b5625f521025ee19f
jedgodsey <jed.godsey@gmail.com>
2020-09-22 23:16:55 -0700

    natural character lifespan finished

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,4 @@
+//Global Variables
 let time = 0;
 let dayNight = 'day';
 let loneliness = 0;
@@ -5,7 +6,9 @@ let hunger = 0;
 let tiredness = 0;
 let calendar = 1;
 let openClose = 0;
+let $raccoon = $('#roscoe');
 
+//Timing functions
 setInterval(() => {
     $('time').text(`One more day with Roscoe makes ${calendar}!`)
     calendar++;
@@ -16,8 +19,9 @@ setInterval(() => {
     loneliness++;
     hunger++;
     tiredness++;
+    biggestStress();
     $('summary').text(`LONELINESS: ${loneliness}  HUNGER: ${hunger}  TIREDNESS: ${tiredness}`)    
-}, 5000);
+}, 1000);
 
 let lightOut = () => {
     if (calendar % 2 == 0) {
@@ -27,7 +31,8 @@ let lightOut = () => {
     }
 }
 
-$('body').on('click', function(event) {
+//Event listeners for game interaction
+$('#door-frame').click(function() {
     if (openClose % 2 == 0) {
         $('#door-img').attr('src', './resources/door-closed.svg');
     } else {
@@ -35,3 +40,67 @@ $('body').on('click', function(event) {
     }
     openClose++
 })
+
+//Mechanics for selecting raccoon image
+
+let gone = () => {
+}
+
+let response = (stressor) => {
+    switch (stressor) {
+        case loneliness:
+            $raccoon.attr('src', './resources/excited.svg');
+            break;
+            case hunger:
+                $raccoon.attr('src', './resources/full.svg');
+                break;
+                case tiredness:
+                    $raccoon.attr('src', './resources/full.svg');
+                }
+            }
+            
+let biggestStress = () => {
+    if (loneliness > 9 || hunger > 9 || tiredness > 9) {
+        ($('figure')).html('');
+    } else {
+        const allStressors = [loneliness, hunger, tiredness];
+        troubleIndex = 0;
+        const stressImages = ['./resources/lonely.svg', './resources/hungry.svg', './resources/tired.svg']
+        if (Math.max(...allStressors) > 6) {
+            let pressingStress = 0;
+            for (let i = 0; i < allStressors.length; i++) {
+                if (allStressors[i] > pressingStress) {
+                    pressingStresss = allStressors[i];
+                    troubleIndex = i;
+                }
+            }
+        }
+        $raccoon.attr('src', stressImages[troubleIndex]);
+    }
+}
+
+//Mechanics for adjusting scores
+let interact = (stressor) => {
+    stressor -= 5;
+    response(stressor);
+}
+
+//////////////////////////////
+$('body').on('click', function(event) {
+    console.log('works');
+})
+
+$('#door-frame').on('click', function(event) {
+    console.log('functions');
+})
+
+// $('.squares').on('click','.square', function(event) {
+//     let $boxColor = $(this).css('background-color');
+//     if ($boxColor == 'rgb(0, 0, 255)') {
+//         points++
+//         $(this).remove();
+//     } else if (points > 0) {
+//         points--;
+//     }
+//     $('h1').text(`Scoreboard: ${points}`);
+// })
